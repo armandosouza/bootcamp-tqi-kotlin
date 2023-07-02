5373bb3d0a0c68e0d33281a3db9263ee100c1a68
Armando Souza <armandosouzafalzi@gmail.com>
2023-07-02 19:06:34 -0300

    Bank class documentation

--- exercicios/src/com/cursojava/banco/Banco.java
+++ exercicios/src/com/cursojava/banco/Banco.java
@@ -1,8 +1,13 @@
 package com.cursojava.banco;
 
+/*
+ * Representação simples de uma conta bancária 
+ * @author Armando Souza
+ * @version 1.0
+ * @since 02/07/2023
+ */
 public class Banco {
     double saldo = 0;
-
     public void sacar(double valor) {
         if(saldo < valor) {
             System.out.println("Saldo insuficiente!");
