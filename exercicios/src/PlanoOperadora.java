fd4061d36726a69c00208cf7bf1b48fd69e64dd6
Armando Souza <armandosouzafalzi@gmail.com>
2023-07-03 01:12:52 -0300

    Operators exercise added

---
+++ exercicios/src/PlanoOperadora.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        final String planoPequeno = "P";
+        final String planoMedio = "M";
+        final String planoGrande = "G";
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite sua opção:");
+        String planoUsuario = scanner.next();
+
+        switch(planoUsuario) {
+            case planoPequeno: {
+                System.out.println("Apenas 500MB de internet");
+                break;
+            }
+            case planoMedio: {
+                System.out.println("2GB de internet e ligações ilimitadas");
+                break;
+            }
+            case planoGrande: {
+                System.out.println("5GB de internet, ligações ilimitadas e SMS ilimitado");
+                break;
+            }
+            default: {
+                System.out.println("Opção inválida");
+            }
+        }
+    }
+}
